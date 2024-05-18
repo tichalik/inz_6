83b382fd13760fbccb63ca2dca78105b496277d6
tichalik <tichalik@gmail.com>
2024-05-18 10:10:47 +0200

    added checking if non-terminals really are read corrrectly

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+	std::cout << "--------------chaining << no storing to string------" << std::endl;
     {
 		//simulated http input
 		std::string http_input = "a b w c w d";
@@ -11,12 +12,29 @@ int main()
 		Http_grammar_adapter http_grammar_adapter(
 			http_input);
 			
+		std::cout << "http_grammar_adapter.nonterminals.symbols:" << std::endl;
+		for (size_t i=0; i<http_grammar_adapter.nonterminals.symbols.size(); i++)
+		{
+			std::cout << "<" << http_grammar_adapter.nonterminals.symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+		
+		std::cout << "http_grammar_adapter.nonterminals.get_symbols:" << std::endl;
+		std::vector<Symbol> symbols =http_grammar_adapter.nonterminals.get_symbols();
+		for (size_t i=0; i<symbols.size(); i++)
+		{
+			std::cout << "<" << symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+			
+			
 		std::cout << "input:  <<" << http_input 
 			<< ">>\nresult: <<" << http_grammar_adapter.nonterminals_to_http() 
 			<< ">>" << std::endl;
 	}
 	std::cout << "-----------------------------------------------------" << std::endl;
 	
+	std::cout << "--------------single << no storing to string------" << std::endl;
 	{
 		//simulated http input
 		std::string http_input = "a b w c w d";
@@ -25,11 +43,29 @@ int main()
 		Http_grammar_adapter http_grammar_adapter(
 			http_input);
 			
+			
+		std::cout << "http_grammar_adapter.nonterminals.symbols:" << std::endl;
+		for (size_t i=0; i<http_grammar_adapter.nonterminals.symbols.size(); i++)
+		{
+			std::cout << "<" << http_grammar_adapter.nonterminals.symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+		
+		std::cout << "http_grammar_adapter.nonterminals.get_symbols:" << std::endl;
+		std::vector<Symbol> symbols =http_grammar_adapter.nonterminals.get_symbols();
+		for (size_t i=0; i<symbols.size(); i++)
+		{
+			std::cout << "<" << symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+			
+			
 		std::cout << http_grammar_adapter.nonterminals_to_http() 
 			<< std::endl;
 	}
 	std::cout << "-----------------------------------------------------" << std::endl;
 	
+	std::cout << "--------------chaining << storing to string------" << std::endl;
 	{
 		//simulated http input
 		std::string http_input = "a b w c w d";
@@ -37,6 +73,23 @@ int main()
 		//http adapter -- filling head and rules with dummies
 		Http_grammar_adapter http_grammar_adapter(
 			http_input);
+			
+			
+		std::cout << "http_grammar_adapter.nonterminals.symbols:" << std::endl;
+		for (size_t i=0; i<http_grammar_adapter.nonterminals.symbols.size(); i++)
+		{
+			std::cout << "<" << http_grammar_adapter.nonterminals.symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+		
+		std::cout << "http_grammar_adapter.nonterminals.get_symbols:" << std::endl;
+		std::vector<Symbol> symbols =http_grammar_adapter.nonterminals.get_symbols();
+		for (size_t i=0; i<symbols.size(); i++)
+		{
+			std::cout << "<" << symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+			
 			
 		std::string res = http_grammar_adapter.nonterminals_to_http() ;
 		
@@ -46,6 +99,7 @@ int main()
 	}
 	std::cout << "-----------------------------------------------------" << std::endl;
 	
+	std::cout << "--------------single << storing to string------" << std::endl;
 	{
 		//simulated http input
 		std::string http_input = "a b w c w d";
@@ -53,6 +107,23 @@ int main()
 		//http adapter -- filling head and rules with dummies
 		Http_grammar_adapter http_grammar_adapter(
 			http_input);
+			
+			
+		std::cout << "http_grammar_adapter.nonterminals.symbols:" << std::endl;
+		for (size_t i=0; i<http_grammar_adapter.nonterminals.symbols.size(); i++)
+		{
+			std::cout << "<" << http_grammar_adapter.nonterminals.symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+		
+		std::cout << "http_grammar_adapter.nonterminals.get_symbols:" << std::endl;
+		std::vector<Symbol> symbols =http_grammar_adapter.nonterminals.get_symbols();
+		for (size_t i=0; i<symbols.size(); i++)
+		{
+			std::cout << "<" << symbols[i].symbol << "> " ;
+		}
+		std::cout << std::endl;
+			
 			
 		std::string res = http_grammar_adapter.nonterminals_to_http() ;
 			
