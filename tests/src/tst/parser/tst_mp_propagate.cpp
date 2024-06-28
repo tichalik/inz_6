a6c795506cd47ab23e7c1fafd066fb27940919ba
tichalik <tichalik@gmail.com>
2024-06-28 16:55:11 +0200

    testst of propagate_parsing_table finished

--- tests/src/tst/parser/tst_mp_propagate.cpp
+++ tests/src/tst/parser/tst_mp_propagate.cpp
@@ -103,5 +103,494 @@ void TST_mod_parser::test_propagate_parsing_table()
 			expected_ptable 
 		);
 	}
+	
+	{
+		std::cout << "===============================================================" << std::endl;
+		std::cout << " simplest ambiguity -- 2 symbols, 2 rules" << std::endl;
+		std::cout << "===============================================================" << std::endl;
+		
+		//only rules matter in parsing 
+		Grammar grammar;
+		
+		{
+			Rule rule;
+			rule.left = "a";
+			rule.right1 = "A";
+			rule.right2 = "B";
+			grammar.rules.push_back(rule);
+		}
+		{
+			Rule rule;
+			rule.left = "b";
+			rule.right1 = "A";
+			rule.right2 = "B";
+			grammar.rules.push_back(rule);
+		}
+		
+		Word word;
+		word.push_back("A");
+		word.push_back("B");
+		
+		PTable expected_ptable(word);
+		PTable_reference child1;
+		child1.x = 0;
+		child1.y = 0;
+		child1.list_index = 0;
+	
+		PTable_reference child2;
+		child2.x = 1;
+		child2.y = 0;
+		child2.list_index = 0;
+	
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][0].push_back(ptable_entry);
+		}
+		
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "b";
+			ptable_entry.visited = false;
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][0].push_back(ptable_entry);
+		}
+		
+		
+		
+		_test_propagate_parsing_table(
+			grammar, 
+			word, 
+			expected_ptable 
+		);
+	}
+	
+	{
+		std::cout << "===============================================================" << std::endl;
+		std::cout << " 3 symbols, 2 rules, left first" << std::endl;
+		std::cout << "===============================================================" << std::endl;
+		
+		//only rules matter in parsing 
+		Grammar grammar;
+		
+		{
+			Rule rule;
+			rule.left = "a";
+			rule.right1 = "A";
+			rule.right2 = "B";
+			grammar.rules.push_back(rule);
+		}
+		{
+			Rule rule;
+			rule.left = "b";
+			rule.right1 = "a";
+			rule.right2 = "C";
+			grammar.rules.push_back(rule);
+		}
+		
+		Word word;
+		word.push_back("A");
+		word.push_back("B");
+		word.push_back("C");
+		
+		PTable expected_ptable(word);
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 0;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 1;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][0].push_back(ptable_entry);
+		}
+		
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "b";
+			ptable_entry.visited = false;
+			
+			
+			PTable_reference child1;
+			child1.x = 0;
+			child1.y = 1;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 2;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[2][0].push_back(ptable_entry);
+		}
+		
+		
+		
+		_test_propagate_parsing_table(
+			grammar, 
+			word, 
+			expected_ptable 
+		);
+	}
+	
+	{
+		std::cout << "===============================================================" << std::endl;
+		std::cout << " 3 symbols, 2 rules, right first" << std::endl;
+		std::cout << "===============================================================" << std::endl;
+		
+		//only rules matter in parsing 
+		Grammar grammar;
+		
+		{
+			Rule rule;
+			rule.left = "a";
+			rule.right1 = "A";
+			rule.right2 = "B";
+			grammar.rules.push_back(rule);
+		}
+		{
+			Rule rule;
+			rule.left = "b";
+			rule.right1 = "C";
+			rule.right2 = "a";
+			grammar.rules.push_back(rule);
+		}
+		
+		Word word;
+		word.push_back("C");
+		word.push_back("A");
+		word.push_back("B");
+		
+		PTable expected_ptable(word);
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 1;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 2;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][1].push_back(ptable_entry);
+		}
+		
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "b";
+			ptable_entry.visited = false;
+			
+			
+			PTable_reference child1;
+			child1.x = 0;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 1;
+			child2.y = 1;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[2][0].push_back(ptable_entry);
+		}
+		
+		
+		
+		_test_propagate_parsing_table(
+			grammar, 
+			word, 
+			expected_ptable 
+		);
+	}
+	
+	{
+		std::cout << "===============================================================" << std::endl;
+		std::cout << " 3 symbols, 2 rules + 1 that produces incomplete parse" << std::endl;
+		std::cout << "===============================================================" << std::endl;
+		
+		//only rules matter in parsing 
+		Grammar grammar;
+		
+		{
+			Rule rule;
+			rule.left = "a";
+			rule.right1 = "A";
+			rule.right2 = "B";
+			grammar.rules.push_back(rule);
+		}
+		{
+			Rule rule;
+			rule.left = "d";
+			rule.right1 = "A";
+			rule.right2 = "B";
+			grammar.rules.push_back(rule);
+		}
+		{
+			Rule rule;
+			rule.left = "b";
+			rule.right1 = "C";
+			rule.right2 = "a";
+			grammar.rules.push_back(rule);
+		}
+		
+		Word word;
+		word.push_back("C");
+		word.push_back("A");
+		word.push_back("B");
+		
+		PTable expected_ptable(word);
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 1;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 2;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][1].push_back(ptable_entry);
+		}
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "d";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 1;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 2;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][1].push_back(ptable_entry);
+		}
+		
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "b";
+			ptable_entry.visited = false;
+			
+			
+			PTable_reference child1;
+			child1.x = 0;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 1;
+			child2.y = 1;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[2][0].push_back(ptable_entry);
+		}
+		
+		
+		
+		_test_propagate_parsing_table(
+			grammar, 
+			word, 
+			expected_ptable 
+		);
+	}
+	
+	{
+		std::cout << "===============================================================" << std::endl;
+		std::cout << " 3 symbols, 2 rules + 1 that produces incomplete parse" << std::endl;
+		std::cout << "===============================================================" << std::endl;
+		
+		//only rules matter in parsing 
+		Grammar grammar;
+		
+		{
+			Rule rule;
+			rule.left = "a";
+			rule.right1 = "A";
+			rule.right2 = "A";
+			grammar.rules.push_back(rule);
+		}
+		{
+			Rule rule;
+			rule.left = "a";
+			rule.right1 = "A";
+			rule.right2 = "a";
+			grammar.rules.push_back(rule);
+		}
+		
+		{
+			Rule rule;
+			rule.left = "a";
+			rule.right1 = "a";
+			rule.right2 = "A";
+			grammar.rules.push_back(rule);
+		}
+		
+		Word word;
+		word.push_back("A");
+		word.push_back("A");
+		word.push_back("A");
+		
+		PTable expected_ptable(word);
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 0;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 1;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][0].push_back(ptable_entry);
+		}
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 1;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 2;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[1][1].push_back(ptable_entry);
+		}
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 0;
+			child1.y = 0;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 1;
+			child2.y = 1;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[2][0].push_back(ptable_entry);
+		}
+		
+		
+		{
+			PTable_entry ptable_entry;
+			ptable_entry.tag = "a";
+			ptable_entry.visited = false;
+				
+			
+			PTable_reference child1;
+			child1.x = 0;
+			child1.y = 1;
+			child1.list_index = 0;
+
+			PTable_reference child2;
+			child2.x = 2;
+			child2.y = 0;
+			child2.list_index = 0;
+
+			
+			ptable_entry.children.push_back(child1);
+			ptable_entry.children.push_back(child2);
+			expected_ptable.tab[2][0].push_back(ptable_entry);
+		}
+		
+		
+		
+		_test_propagate_parsing_table(
+			grammar, 
+			word, 
+			expected_ptable 
+		);
+	}
+	
+	//symbol with list_index other than 0
+	//interlocking trees 
 }
 
