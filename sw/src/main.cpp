e958851c0b93bbd3b25c2a8cfbec0e1d10adc35f
tichalik <tichalik@gmail.com>
2024-10-11 12:37:20 +0200

    fixed examples, added new
    removed html files

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -7,12 +7,7 @@ int main()
 	Server::init();
 	Server::set_dummy_get("./src/html_templates/tst.html", "tst");
 	
-	Server::set_dummy_get("./src/html_templates/tst1.html", "tst1");
-	Server::set_dummy_get("./src/html_templates/tst2.html", "tst2");
-	Server::set_dummy_get("./src/html_templates/tst3.html", "tst3");
-	Server::set_dummy_get("./src/html_templates/tst4.html", "tst4");
 	Server::set_dummy_get("./src/html_templates/tst_html_entities_errors.html", "tst_html_entities_errors");
-	Server::set_dummy_get("./src/html_templates/tst_html_parse.html", "tst_html");
 	Server::run();
 
 }
